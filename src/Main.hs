6f36bf5a1cec92574162a50a815895307ba16c28
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-06-30 19:58:20 +0300

    Visualizing normals (in a weird way)

--- src/Main.hs
+++ src/Main.hs
@@ -15,23 +15,30 @@ import qualified Ray as R
 main :: IO ()
 main = writeFile "output.ppm" mkPpmFile
 
-hitSphere :: Ray -> Vec3 -> Double -> Bool
-hitSphere ray center radius = 
-  let dir = R.direction ray
-      oc = (R.origin ray) .- center
-      a = V.dot dir dir
-      b = 2.0 * V.dot oc dir 
-      c = V.dot oc oc - radius * radius 
-      discriminant = b * b - 4 * a * c
-      in discriminant > 0
+hitSphere :: Ray -> Vec3 -> Double -> Double
+hitSphere ray center radius 
+  | discriminant < 0 = -1.0
+  | otherwise = (-b - sqrt discriminant) / 2.0 * a
+  where
+    dir = R.direction ray
+    oc = R.origin ray .- center
+    a = V.dot dir dir
+    b = 2.0 * V.dot oc dir 
+    c = V.dot oc oc - radius * radius 
+    discriminant = b * b - 4 * a * c
 
 color :: Ray -> Vec3
-color r 
-  | hitSphere r (0, 0, -1) 0.5 = (1.0, 0, 0)
-  | otherwise = let 
-    unitDirection = V.mkUnitVec3 (R.direction r)
-    t = 0.5 * (V.y unitDirection + 1.0)
-    in (1.0, 1.0, 1.0) .** (1.0 - t) .+ (0.5, 0.7, 1.0) .** t
+color r =
+  let t = hitSphere r (0, 0, -1) 0.5
+   in 
+    if t > 0.0
+      then
+        let n = V.mkUnitVec3 (R.pointAt r t .- (0, 0, -1))
+        in (V.x n + 1, V.y n + 1, V.z n + 1) .** 0.5
+      else 
+        let unitDirection = V.mkUnitVec3 (R.direction r)
+            t = 0.5 * (V.y unitDirection + 1.0)
+        in (1.0, 1.0, 1.0) .** (1.0 - t) .+ (0.5, 0.7, 1.0) .** t
 
 mkPpmFile :: String
 mkPpmFile = 
@@ -64,15 +71,15 @@ mkPpmFile =
     mkLine :: Int -> Int -> String
     mkLine i j = 
       let 
-        u = (fromIntegral i) / fromIntegral(width) :: Double
-        v = (fromIntegral j) / fromIntegral(height) :: Double
+        u = fromIntegral i / fromIntegral width :: Double
+        v = fromIntegral j / fromIntegral height :: Double
 
         r = Ray origin (lowerLeftCorner .+ horizontal .** u .+ vertical .** v)
         col = color r
 
-        ir = 255.99 * (V.r col)
-        ig = 255.99 * (V.g col)
-        ib = 255.99 * (V.b col)
+        ir = 255.99 * V.r col
+        ig = 255.99 * V.g col
+        ib = 255.99 * V.b col
 
         -- val :: Integer
         val = ((mconcat . intersperse " " . map (show . floor)) [ir, ig, ib])
