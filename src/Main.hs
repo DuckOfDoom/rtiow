f1d873d143dc8b6fba98fbf2bc90b5f560416831
Vladimir Yanovsky <horaxlfa@gmail.com>
2019-07-03 10:14:03 +0300

    Fixed antialiasing!

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import           System.IO     (writeFile)
 import           System.Random (randomRIO)
+import           Control.Monad 
 
 import           Data.List     (intersperse)
 import           Data.Monoid   (mconcat)
@@ -12,7 +13,7 @@ import           Camera        as C (getRay, mkDefaultCamera)
 import           Hitable       (HitRecord (..), Hitable (..))
 import           Ray           (Ray (..))
 import           Sphere        (Sphere (..))
-import           Vec3          (Vec3, (.**), (.+), (.-), (.//))
+import           Vec3          (Vec3, (.**), (.+), (.//))
 
 import qualified Ray           as R
 import qualified Vec3          as V
@@ -22,19 +23,6 @@ import Debug.Trace
 
 main :: IO ()
 main = mkPpmFile >>= writeFile "output.ppm"
--- main = writeFile "output.ppm" mkPpmFile'
-
-hitSphere :: Ray -> Vec3 -> Double -> Double
-hitSphere ray center radius
-  | discriminant < 0 = -1.0
-  | otherwise = (-b - sqrt discriminant) / 2.0 * a
-  where
-    dir = R.direction ray
-    oc = R.origin ray .- center
-    a = V.dot dir dir
-    b = 2.0 * V.dot oc dir
-    c = V.dot oc oc - radius * radius
-    discriminant = b * b - 4 * a * c
 
 -- gets a color by tracing a ray against a list of hitable objects
 color :: Hitable a => Ray -> [a] -> Vec3
@@ -43,10 +31,9 @@ color r hitables =
     Just (HitRecord _ _ normal) -> -- trace ("hit! " <> show r) $
       (V.x normal + 1, V.y normal + 1, V.z normal + 1)  .** 0.5
     Nothing -> 
-      (0.0, 0.0, 0.0)
-        -- let unitDirection = V.mkUnitVec3 (R.direction r)
-        --     t = 0.5 * (V.y unitDirection + 1.0)
-        -- in (1.0, 1.0, 1.0) .** (1.0 - t) .+ (0.5, 0.7, 1.0) .** t
+        let unitDirection = V.mkUnitVec3 (R.direction r)
+            t = 0.5 * (V.y unitDirection + 1.0)
+        in (1.0, 1.0, 1.0) .** (1.0 - t) .+ (0.5, 0.7, 1.0) .** t
 
 mkPpmFile :: IO String
 mkPpmFile = do
@@ -80,27 +67,27 @@ mkPpmFile = do
 
     mkLine :: Int -> Int -> IO String
     mkLine i j = do
-      pColor <- getPixelColor i j
-      pure ((mconcat . intersperse " " . map (show . floor)) (mkColorAsList pColor))
+      pColor <- getAverageColor i j
+      pure $ formatColor pColor
         where
-          -- Runs a raytrace for each pixel multiple times then averages values
           getAverageColor :: Int -> Int -> IO Vec3
           getAverageColor i j = do
-            list <- sequence $ replicate antialiasingPassCount (getPixelColor i j)
-            pure $ (foldl1 (.+) list) .// (fromIntegral antialiasingPassCount)
+            list <- replicateM antialiasingPassCount (getPixelColor i j)
+            pure $ foldl1 (.+) list .// fromIntegral antialiasingPassCount
 
           -- Runs a raytrace with random displacement to calculate antialiased pixel color
           getPixelColor :: Int -> Int -> IO Vec3
           getPixelColor i j = do
             u <- do
-              r <- (randomRIO (0.0, 1.0))
-              pure (fromIntegral i + r / fromIntegral width :: Double)
+              r <- randomRIO (0.0, 1.0)
+              pure ((fromIntegral i + r) / fromIntegral width :: Double)
             v <- do
-              r <- (randomRIO (0.0, 1.0))
-              pure (fromIntegral j + r / fromIntegral height :: Double)
+              r <- randomRIO (0.0, 1.0)
+              pure ((fromIntegral j + r) / fromIntegral height :: Double)
             pure $ color (C.getRay camera u v) hitableList
 
-          mkColorAsList vec =
+          formatColor :: Vec3 -> String
+          formatColor vec = mconcat . intersperse " " . map (show .  (floor :: Double -> Integer)) $
             [ 255.99 * V.r vec
             , 255.99 * V.g vec
             , 255.99 * V.b vec
